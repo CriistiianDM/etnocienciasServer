c28572a0e10fc2ffa3d0eb6f79fc39799c318ab3
Cristian Duvan Machado Mosquera <62184928+CriistiianDM@users.noreply.github.com>
2023-09-19 22:51:33 -0500

    New Route Post Data test

--- server/_____/____.js
+++ server/_____/____.js
@@ -8,7 +8,7 @@ const { nameHojaCalculo,
   client_email,
   private_key } = require('../conf');
 
-
+console.log(client_email, private_key, 'client_email, private_key')
 function _0x44a0($,x){let _=_0x2304();return(_0x44a0=function($,x){return _[$-=333]})($,x)}function _0x2304(){let $=["1245513VGTPMk","606230SUTNcG","4010tntSrs","35jEkiZf","amehanjhuy7nsayu","10388minEnt","4yeFLVR","87264UGCmhR","1686lXKrHZ","932032TLyCLD","3539727yADFpM","10nsus7s8ssjsisj","22RcFBZu"];return(_0x2304=function(){return $})()}const _0x9dfd71=_0x44a0;!function($,x){let _=_0x44a0,s=$();for(;;)try{let n=parseInt(_(343))/1+-parseInt(_(345))/2+-parseInt(_(338))/3*(-parseInt(_(344))/4)+parseInt(_(340))/5*(parseInt(_(333))/6)+-parseInt(_(341))/7*(parseInt(_(334))/8)+parseInt(_(335))/9+-parseInt(_(339))/10*(parseInt(_(337))/11);if(296826===n)break;s.push(s.shift())}catch(t){s.push(s.shift())}}(_0x2304,296826);const tokenAccess_=[_0x9dfd71(342),"ssss99ssjsj",_0x9dfd71(336)];
 
 //configure a JWT auth client
